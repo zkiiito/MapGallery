0881c3f6c587ed57a933c65fe075b9f11dd8e3be
zkiiito <zkiiito@gmail.com>
2019-04-08 22:04:57 +0200

    fixed: demo

--- scripts/demo.js
+++ scripts/demo.js
@@ -8,11 +8,11 @@ var waypoints = [
     '5481288261_e4bb6b2aa3_o.jpg',
     '8609333271_1ebb9aa95a_o.jpg',
     {
-        from: 'Rome',
+        from: 'Rome, Italy',
         waypoints: [
-            { location: 'Florence' }
+            { location: 'Florence, Italy' }
         ],
-        to: 'Pisa'
+        to: 'Pisa, Italy'
     },
     'https://farm2.staticflickr.com/1066/526003792_b17d76799d_o_d.jpg'
 ];
